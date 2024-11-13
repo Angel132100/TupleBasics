80d9988f7dc64b7c8256cb42754e8b8d44cdf0f5
Angel Parker <parkeang000@warren.k12.in.us>
2024-11-13 10:03:30 -0500

    commit

---
+++ main.py
@@ -0,0 +1,36 @@
+# SECTION 1
+
+fruit_tuple = ("apple", "banana", "cherry", "date")
+
+print(fruit_tuple[-1])  # TODO: Print the last element
+
+
+
+slice_tuple = fruit_tuple[1:3]  # TODO: Slice to get banana and cherry
+
+print(slice_tuple)
+
+
+
+# SECTION 2
+
+num_tuple = (1, 2, 3, 4, 2)
+
+print(num_tuple.count(2))  # TODO: Count occurrences of 2
+
+print(num_tuple.index(3))  # TODO: Find index of 3
+
+
+
+# SECTION 3
+
+person_info = ("John", 25, "Engineer")  # TODO: Create person_info tuple
+
+name, age, job = person_info  # TODO: Unpack the tuple
+
+print(name)
+
+print(age)
+
+print(job)
+
